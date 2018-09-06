28dcc9d762699c207dd0db9daf1b78f78749fccb
Bailey Srimoungchanh <srimoungchanh.bailey@gmail.com>
2018-09-06 17:14:45 -0500

    Added example of adding an icon to JButtons, labeled JButtons to help further design the grid.

--- Board.java
+++ Board.java
@@ -1,21 +1,23 @@
-import java.awt.EventQueue;
-import java.awt.GridLayout;
+import java.lang.Exception;
+import java.awt.*;
 import javax.swing.*;
+import javax.imageio.*;
 
 public class Board {
+    /* Centralized location for constants */
+    public static int tileSize = 30;
 
-    public Board( int row, int height, int mines ) {
+    public Board(int numTiles, int mines) {
         /* EventQueue.invokeLater() is necessary to avoid window hanging */
         EventQueue.invokeLater(() -> {
-            initGame( row, height, mines );
+            initGame(numTiles, mines);
         });
     }
 
-    private void initGame( int tileRow, int tileHeight, int mines ) {
+    private void initGame(int numTiles, int mines) {
 
         /* Constants for board size based on 15 x 15 pixel tiles */
-        final int rowSize = ( tileRow * 20 );
-        final int rowHeight = ( tileHeight * 20 );
+        final int windowSize = (numTiles * tileSize);
 
         /* JFrame game is the board window */
         JFrame game = new JFrame();
@@ -24,29 +26,42 @@ public class Board {
         Below are the JFrame values being set, documented by the related
         function name used to set the JFrame characteristic
         */
-        game.setTitle( "Definitely not Minesweeper" );
-        game.setSize( rowSize, rowHeight );
-        game.setLayout( new GridLayout( tileRow, tileHeight ) );
-        game.setLocationRelativeTo( null );
-        game.setResizable( false );
-        game.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE );
-        game.setVisible( true );
+        game.setTitle("Definitely not Minesweeper");
+        game.setSize(windowSize, windowSize);
+        game.setLayout(new GridLayout(numTiles, numTiles));
+        game.setLocationRelativeTo(null);
+        game.setResizable(false);
+        game.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         /*
         This adds the tiles to the grid, in this case the number
-        of tiles will be the tileRow * tileHeight.
+        of tiles will be the tileRow * tileHeight. The tile icon will be
+        changed throughout the coding process, for now Number1.png is just
+        an example icon I made in microsoft paint to display in the grid.
         */
-        for(int i = 0; i < tileRow * tileHeight; i++){
-            game.add( new JButton( "x" ) );
+        try {
+//            Image img = ImageIO.read(getClass().getResource("Resources/Number1.png"));
+            for (int i = 0; i < numTiles * numTiles; i++) {
+                JButton newButton = new JButton();
+                newButton.setMargin(new Insets(0, 0, 0, 0));
+//                newButton.setIcon( new ImageIcon(img) );
+                newButton.setText(Integer.toString(i));
+                newButton.setPreferredSize(new Dimension(tileSize, tileSize));
+                game.add(newButton);
+            }
+        } catch (Exception ex) {
+            System.out.println("Problem with loading Resources/Number1.png");
         }
 
+        game.validate();
+        game.setVisible(true);
         /*
         This WindowListener has an Overridden windowClosing event that allows
         the function Menu.open() to get called on the Board window closing.
         */
-        game.addWindowListener( new java.awt.event.WindowAdapter() {
+        game.addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
-            public void windowClosing( java.awt.event.WindowEvent e ){
+            public void windowClosing(java.awt.event.WindowEvent e) {
                 Menu.open();
             }
         });
--- Menu.java
+++ Menu.java
@@ -3,58 +3,71 @@ import java.awt.*;
 public class Menu {
 
     /*Menu() Constructor
-    * Constructor just calls initMenu() which
-    * initializes the menu
-    * */
-    public Menu(){
+     * Constructor just calls initMenu() which
+     * initializes the menu
+     * */
+    public Menu() {
+
         initMenu();
     }
 
     /*open()
-    * @Return Void
-    * Sort of like a get/set function but in this
-    * case open() provides a public method to avoid
-    * creating Menu Objects in any other class
-    * */
-    public static void open(){
+     * @Return Void
+     * Sort of like a get/set function but in this
+     * case open() provides a public method to avoid
+     * creating Menu Objects in any other class
+     * */
+    public static void open() {
         Menu menu = new Menu();
     }
 
     /*initMenu()
-    * @Return Void
-    * Creates the menu, called only by the constructor
-    * */
-    private void initMenu(){
+     * @Return Void
+     * Creates the menu, called only by the constructor
+     * */
+    private void initMenu() {
         System.out.println("initMenu()");
     }
 
-    public static void main(String[] args){
+    /*Menu.main(String[] args)
+     * Main function of Menu, just used for board
+     * creation testing at the moment, will probably be
+     * in future implementations.
+     * */
+    public static void main(String[] args) {
         /*Currently using this commented block to test the
-        * Board and Menu class while an entrance function/driver class
-        * has not been created/determined*/
+         * Board and Menu class while an entrance function/driver class
+         * has not been created/determined*/
 
 
-        /*Row and Col represent the board width x height the user will
-        * enter, the if statements immediately below them are an
-        * example of using the user screenSize to respond to their
-        * input. If we have 20x20 pixel tiles and they want a 30 x 20
-        * board, check their height and width to see if their screenSize width
-        * and height is greater than the input board size.*/
+        /* Current implementation due to the nature of gridlayout partitioning the window
+         * size to the number of cells, we will make only square windows to allow the tiles
+         * to be of appropriate sizing. Below is getting the screensize information and
+         * using it in a pseudo error handling manner, this will eventually be moved to a
+         * max input value the user can use to create a board.*/
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int width = (int)screenSize.getWidth();
-        int height = (int)screenSize.getHeight();
-        System.out.println( "Width: " + width );
-        System.out.println( "Height: " + height );
+        int width = (int) screenSize.getWidth();
+        int height = (int) screenSize.getHeight();
+        int windowDimensionMax = (width <= height) ? width : height;
+        System.out.println("Width: " + width);
+        System.out.println("Height: " + height);
 
-        int row = 100;
-        int col = 50;
+        /* board will be displayed as tile x tile board where boardsize is the tiles
+         * along one side of the board, this will be changed to user input */
+        int boardSize = 28;
 
-        if( row * 20 >= height ) {
-            System.out.println("Too many rows");
-            System.exit(0); }
-        if( col * 20 >= width ){
-            System.out.println("Too many columns");
-            System.exit(0); }
-        Board game = new Board( row, col, 15 );
+        /* Check if board fits screen, if not print corresponding limiting factor and terminate
+         * This will hopefully be used to implement a max value the user can input into the game */
+        if (((boardSize * Board.tileSize) >= windowDimensionMax)) {
+            if (boardSize * Board.tileSize >= height) {
+                System.out.println("Window is too tall for this screen.");
+            }
+            if (boardSize * Board.tileSize >= width) {
+                System.out.println("The window is too wide for this screen.");
+            }
+            System.exit(0);
+        }
+
+        Board game = new Board(boardSize, 15);
     }
 }
