b9a124e98032f4093b11dc8719751e76e1aa18e2
Devin Suttles <devinfnsuttles@gmail.com>
2018-10-05 12:19:20 -0500

    Change Sound.java

--- minesweeper/Sound.java
+++ minesweeper/Sound.java
@@ -1,21 +1,29 @@
+package minesweeper;
+
 import java.io.*;
+import java.net.URL;
 import javax.sound.sampled.*;
 public class Sound
 {
-  Sound()
-  {
-  }
-  static void play()
-  {
-    try
-    {
-    Clip clip = AudioSystem.getClip();
-    clip.open(AudioSystem.getAudioInputStream(new File("Resources/bomb.wav")));
-    clip.start();
-    }
-    catch (Exception exc)
-    {
-    exc.printStackTrace(System.out);
-    }
-  }
+	Sound()
+	{
+	}
+	static void playBomb()
+	{
+		URL url = Sound.class.getResource("bomb.wav");
+		try
+	    {
+	    Clip clip = AudioSystem.getClip();
+	    clip.open(AudioSystem.getAudioInputStream(new File( url.toURI())));
+	    clip.start();
+	    }
+	    catch (Exception exc)
+	    {
+	    exc.printStackTrace(System.out);
+	    }
+	}
+	void placingFlag()
+	{
+		//class will implement a sound or slap type of noise 
+	}
 }
